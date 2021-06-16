e229652a8cd41f6cf63f1da66e48b9df8da5fecb
AD <TolikDianov@gmail.com>
2021-06-16 17:50:07 +0300

    test1

--- TestAdd_Group.py
+++ TestAdd_Group.py
@@ -14,7 +14,7 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        wd.get("https://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
