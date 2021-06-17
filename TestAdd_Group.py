ece1fe483d767383e3050658d5e2c3f144fd17e6
AD <TolikDianov@gmail.com>
2021-06-17 17:15:36 +0300

    Вспомогательные классы

--- TestAdd_Group.py
+++ TestAdd_Group.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 
 class TestAddGroup(unittest.TestCase):
@@ -14,44 +15,22 @@ class TestAddGroup(unittest.TestCase):
         wd = self.open_home_page()
         self.login(wd, username="admin", password="secret")
         self.open_groups(wd)
-        self.create_group(wd, name="test", header="test", footer="test")
+        self.create_group(wd, Group(name="test", header="test", footer="test"))
         self.return_to_group_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
-            wd = self.open_home_page()
-            self.login(wd, username="admin", password="secret")
-            self.open_groups(wd)
-            self.create_group(wd, name="", header="", footer="")
-            self.return_to_group_page(wd)
-            self.logout(wd)
+        wd = self.open_home_page()
+        self.login(wd, username="admin", password="secret")
+        self.open_groups(wd)
+        self.create_group(wd, Group(name="", header="", footer=""))
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-
-    def create_group(self, wd, name, header, footer):
-        # init_groups_creation
-        wd.find_element_by_name("new").click()
-        #fill_group_form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
-        #submit_group_creation
-        wd.find_element_by_name("submit").click()
-
-
-    def open_groups(self, wd):
-        wd.find_element_by_link_text("groups").click()
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook")
+        return wd
 
     def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
@@ -61,10 +40,31 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook")
-        return wd
+    def open_groups(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, Group):
+        # init_groups_creation
+        wd.find_element_by_name("new").click()
+        #fill_group_form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(Group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(Group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(Group.footer)
+        #submit_group_creation
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
