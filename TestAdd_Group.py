271f28febe5d713206c023e3294daedfbc7b3a04
AD <TolikDianov@gmail.com>
2021-06-22 14:48:41 +0300

    Добовление нового контакта1

--- TestAdd_Group.py
+++ TestAdd_Group.py
@@ -12,21 +12,22 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
     
     def test_add_group(self):
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="test", header="test", footer="test"))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="test", header="test", footer="test"))
+        self.logout()
 
     def test_add_empty_group(self):
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=""))
+        self.logout()
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook")
         return wd
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
         wd = self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -35,11 +36,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_groups(self, wd):
+    def open_groups(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, Group):
-        self.open_groups(wd
+    def create_group(self, Group):
+        wd = self.wd
+        self.open_groups()
         # init_groups_creation
         wd.find_element_by_name("new").click()
         #fill_group_form
@@ -54,12 +57,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(Group.footer)
         #submit_group_creation
         wd.find_element_by_name("submit").click()
-        self.return_to_group_page(wd
+        self.return_to_group_page()
 
-    def return_to_group_page(self, wd):
+    def return_to_group_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self, wd):
+    def logout(self,):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
 
