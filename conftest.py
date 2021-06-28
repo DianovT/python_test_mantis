73a4d8d17a7377ad7f53bbac2f9c9cc9a10565ac
dianov <Dianov@inventive.ru>
2021-06-28 18:02:21 +0300

    Повышаем "интеллектуальность" функций login и logout: проверка текущего состояния сессии

--- conftest.py
+++ conftest.py
@@ -9,18 +9,17 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.insure_login(username="admin", password="secret")
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.insure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
