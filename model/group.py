9570a4c5499bdf3867680dadbbffe3ec564c24c4
AD <TolikDianov@gmail.com>
2021-07-08 19:59:58 +0300

    Проверка равенства объектов и списков

--- model/group.py
+++ model/group.py
@@ -11,3 +11,6 @@ class Group:
 
     def __repr__(self):
         return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
