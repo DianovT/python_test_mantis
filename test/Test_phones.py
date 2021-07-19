eb249951d0341efe8d9770443117dbe5bb4b368d
AD <TolikDianov@gmail.com>
2021-07-19 15:38:01 +0300

    Тест сравнивания теефонов

---
+++ test/Test_phones.py
@@ -0,0 +1,23 @@
+from model.contact import Сontact
+
+
+import re
+
+def test_phones_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.home_phone == clear(contact_from_edit_page.home_phone)
+    assert contact_from_home_page.mob_phone == clear(contact_from_edit_page.mob_phone)
+    assert contact_from_home_page.work_phone == clear(contact_from_edit_page.work_phone)
+    assert contact_from_home_page.second_phone == clear(contact_from_edit_page.second_phone)
+
+def test_phones_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
+    assert contact_from_view_page.mob_phone == contact_from_edit_page.mob_phone
+    assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
+    assert contact_from_view_page.second_phone == contact_from_edit_page.second_phone
+
+def clear(s):
+    return re.sub("[() -]", "", s)
