c6cd2141269d5f2c50a539ce7e013aad65243f93
dianov <Dianov@inventive.ru>
2021-07-27 14:40:29 +0300

    Ограничения или условия отбора загружаемых данных

--- conftest.py
+++ conftest.py
@@ -41,6 +41,7 @@ def db(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 
 def pytest_generate_tests(metafunc):
@@ -68,6 +69,11 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
 
 
 
