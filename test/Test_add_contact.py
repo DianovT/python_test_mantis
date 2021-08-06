890d1ededdd527e71acb462fcbb388c6a6295819
AD <TolikDianov@gmail.com>
2021-08-06 11:23:54 +0300

    Задание №20: Переделать тесты, чтобы сравнивались данные, загруженные из БД

--- test/Test_add_contact.py
+++ test/Test_add_contact.py
@@ -9,5 +9,6 @@ def test_add_contact(app, db, check_ui, json_contacts):
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = db.get_contact_list()
     old_contacts.append(contact)
+    assert old_contacts == new_contacts
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
